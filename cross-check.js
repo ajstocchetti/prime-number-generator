588598acae2cb915c836d52383e21e654e1dbb4e
tackypinkflamingos <ajstocchetti@gmail.com>
2019-07-02 23:40:35 -0500

    update test

--- cross-check.js
+++ cross-check.js
@@ -1,14 +1,13 @@
 const {bruteForcePrimes} = require('./prime-stream');
 const {primeStreamAdvanced} = require('./advanced-prime-stream');
 
-const numIterations = 200;
-const startAt = 2;
+const numIterations = 2000;
 
 const timeToMs = time => time[0] * 1e9 + time[1];
 const timeDiff = (t1, t2) => timeToMs(t1) - timeToMs(t2);
 
-const regular = bruteForcePrimes(startAt);
-const advanced = primeStreamAdvanced(4, startAt);
+const regular = bruteForcePrimes();
+const advanced = primeStreamAdvanced(4);
 
 
 for (let x = 0; x < numIterations; x++) {
@@ -18,5 +17,5 @@ for (let x = 0; x < numIterations; x++) {
   const ts2 = process.hrtime();
   const b = advanced.next().value;
   tb = process.hrtime(ts2);
-  console.log(a, b, a == b, timeToMs(ta), timeToMs(tb), timeDiff(ta, tb));
+  console.log(a, b, a == b, timeDiff(ta, tb));
 }
