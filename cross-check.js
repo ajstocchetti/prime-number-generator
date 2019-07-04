708adbc9b1e83c60924308d268577316951fadcc
tackypinkflamingos <ajstocchetti@gmail.com>
2019-07-04 07:05:04 -0500

    update validator

--- cross-check.js
+++ cross-check.js
@@ -2,23 +2,26 @@ const {bruteForcePrimes} = require('./prime-stream');
 const {primeStreamAdvanced} = require('./advanced-prime-stream');
 
 const numIterations = 50000000;
-const startLogging = numIterations / 2;
+// const startLogging = numIterations / 20;
+const startLogging = 6000000;
 
 const timeToMs = time => time[0] * 1e9 + time[1];
 const timeDiff = (t1, t2) => timeToMs(t1) - timeToMs(t2);
 
-const regular = bruteForcePrimes();
-const advanced = primeStreamAdvanced(10);
+const bf = bruteForcePrimes();
+const advanced = primeStreamAdvanced(8);
 
 
 for (let x = 0; x < numIterations; x++) {
   const tStart = process.hrtime();
-  const a = regular.next().value;
+  const a = advanced.next().value; // advanced
   ta = process.hrtime(tStart);
+
   const ts2 = process.hrtime();
-  const b = advanced.next().value;
+  const b = bf.next().value; // brute force
   tb = process.hrtime(ts2);
+
   if (a !== b) console.log(x, a, b);
-  if (x > startLogging) console.log(x, timeDiff(ta, tb));
+  if (x > startLogging) console.log(x, a, timeDiff(tb, ta));
   // console.log(x, a, b, a == b, timeDiff(ta, tb));
 }
